5b89067cf1e4ea7f9594ccf7462ff5b9b851a0e2
Dmitriy Lupachev <soadscod@gmail.com>
2020-11-24 18:36:43 +0300

    Add worked ClearSelenideTest

--- src/test/java/github/issuecreate/ClearSelenideTest.java
+++ src/test/java/github/issuecreate/ClearSelenideTest.java
@@ -1,7 +1,13 @@
 package github.issuecreate;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selectors;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class ClearSelenideTest {
@@ -23,6 +29,24 @@ public class ClearSelenideTest {
         $(".header-search-input").val("SoaD-git/QA_lesson_4");
         $(".header-search-input").submit();
 
+        $(By.linkText("SoaD-git/QA_lesson_4")).click();
+        $("[aria-label=\"Create new…\"]").click();
+        $("[data-ga-click=\"Header, create new issue\"]").click();
+
+        $("#issue_title").val("TestIssue");
+        $(byText("Assignees")).click();
+        $("#assignee-filter-field").val("SoaD-git");
+        $(".select-menu-item").$(byText("SoaD-git")).click();
+        $(".select-menu-item").sendKeys(Keys.ESCAPE);
+
+        $(byText("Labels")).click();
+        $("#label-filter-field").val("bug");
+        $(".label-select-menu-item").$(byText("bug")).click();
+        $(".label-select-menu-item").sendKeys(Keys.ESCAPE);
+        $(byText("Submit new issue")).click();
+
+        open("https://github.com/SoaD-git/QA_lesson_4/issues");
+        $(withText("TestIssue")).shouldHave(Condition.exist);
 
 
     }
